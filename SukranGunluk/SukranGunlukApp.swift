2f42bc54fd5fddcf4e037c62043627278a0aa5e0
Mustafa Cakir <98mustafacan@gmail.com>
2024-01-26 15:06:11 +0300

    Initial Commit

---
+++ SukranGunluk/SukranGunlukApp.swift
@@ -0,0 +1,17 @@
+//
+//  SukranGunlukApp.swift
+//  SukranGunluk
+//
+//  Created by MustafaCan on 26.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct SukranGunlukApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
